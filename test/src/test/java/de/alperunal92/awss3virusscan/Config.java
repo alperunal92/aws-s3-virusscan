845a7239cc7366fa7fe13e04a53004f6da08fb6a
Alper Ünal <alper.unal92@hotmail.com>
2020-05-07 01:53:35 +0300

    Add files via upload

---
+++ test/src/test/java/de/alperunal92/awss3virusscan/Config.java
@@ -0,0 +1,52 @@
+package de.alperunal92.awss3virusscan;
+
+public final class Config {
+
+    public enum Key {
+        TEMPLATE_DIR("TEMPLATE_DIR"),
+        IAM_ROLE_ARN("IAM_ROLE_ARN"),
+        DELETION_POLICY("DELETION_POLICY", "delete"),
+        INFECTED_FILES_BUCKET_NAME("INFECTED_FILES_BUCKET_NAME"),
+        INFECTED_FILES_BUCKET_REGION("INFECTED_FILES_BUCKET_REGION");
+
+        private final String name;
+        private final String defaultValue;
+
+        Key(String name, String defaultValue) {
+            this.name = name;
+            this.defaultValue = defaultValue;
+        }
+
+        Key(String name) {
+            this.name = name;
+            this.defaultValue = null;
+        }
+    }
+
+    public static String get(final Key key) {
+        final String env = System.getenv(key.name);
+        if (env == null) {
+            if (key.defaultValue == null) {
+                throw new RuntimeException("config not found: " + key.name);
+            } else {
+                return key.defaultValue;
+            }
+        } else {
+            return env;
+        }
+    }
+
+    public static boolean has(final Key key) {
+        final String env = System.getenv(key.name);
+        if (env == null) {
+            if (key.defaultValue == null) {
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return true;
+        }
+    }
+
+}
